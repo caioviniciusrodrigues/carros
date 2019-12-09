db891e9bcf2b00eb2666df989cda10b004aa318a
caio359 <caio359@BA-NT371.ica.com>
2019-12-09 15:28:31 -0200

    v. 201912091428

--- apicarros/src/main/java/br/com/caio/apicarros/apicarros/IndexController.java
+++ apicarros/src/main/java/br/com/caio/apicarros/apicarros/IndexController.java
@@ -1,6 +1,7 @@
 package br.com.caio.apicarros.apicarros;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,15 @@ public class IndexController {
 	@GetMapping
 	public String hello() {
 		return "Hello";
+	}
+	
+	@GetMapping("teste")
+	public String hello1() {
+		return "Hello";
+	}
+	
+	@GetMapping("/{id}")
+	public String porId(@PathVariable("id") String id) {
+		return "<b>Id:</b> " + id;
 	}
 }
